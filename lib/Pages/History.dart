401547b1fdecbff3b5f5fa823999a181780d2294
Frida Sofía Andrade Sierra <frisoandrade@gmail.com>
2023-08-15 13:08:02 -0600

    history page modificada

--- lib/Pages/History.dart
+++ lib/Pages/History.dart
@@ -1,278 +1,172 @@
 import 'package:flutter/material.dart';
 import 'package:kabanta_app1/variables.dart';
 
-class History extends StatefulWidget {
+class History extends StatelessWidget {
   const History({super.key});
 
   @override
-  State<History> createState() => _HistoryState();
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: HistoryContent(),
+      ),
+    );
+  }
 }
 
-class _HistoryState extends State<History> {
+class HistoryContent extends StatefulWidget {
+  const HistoryContent({
+    super.key,
+  });
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+  State<HistoryContent> createState() => _HistoryContentState();
+}
+
+class _HistoryContentState extends State<HistoryContent> {
+  bool isButtonActive = true;
+  List<ExpansionTile> expansionTiles = [];
+
+  int index = 0;
+
+  @override
+  void toggleButtonColor() {
+    setState(() {
+      isButtonActive = !isButtonActive;
+    });
+  }
+
+  void removeExpansionTile(int index) {
+    setState(() {
+      if (index >= 0 && index < expansionTiles.length) {
+        expansionTiles.removeAt(index);
+      }
+    });
+  }
+
+  void createExpansionTile() {
+    int index = expansionTiles.length + 1;
+    setState(() {
+      expansionTiles.add(
+        ExpansionTile(
+          title: Text('Expansion Tile $index'),
           children: [
-            //0
-            Container(
-              height: 50,
-              color: null,
-            ),
-            //1
-            Container(
-              height: 60,
-              width: MediaQuery.of(context).size.width,
-              //color: Colors.pink.shade100,
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                        child: ElevatedButton(
-                            onPressed: () {}, child: Text('History'))),
-                    SizedBox(
-                        child: ElevatedButton(
-                            onPressed: () {}, child: Text('Program'))),
-                  ],
-                ),
-              ),
-            ),
-            //2
-            Expanded(
-              child: SingleChildScrollView(
-                child: Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      //1
-                      const Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Text(
-                              '0',
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Text(
-                              '1',
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Text(
-                              '2',
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Text(
-                              '3',
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Text(
-                              '4',
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Text(
-                              '5',
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Text(
-                              '6',
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Text(
-                              '7',
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Text(
-                              '8',
-                              style: labelStyle,
-                            ),
-                          ),
-                        ],
-                      ),
-                      //2
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              textName1,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              textName2,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              textName3,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              textName4,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              textName5,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              textName6,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              textName7,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              textName8,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              textName9,
-                              style: labelStyle,
-                            ),
-                          ),
-                        ],
-                      ),
-                      //3
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              time1,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              time2,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              time3,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              time4,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              time5,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              time6,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              time7,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              time8,
-                              style: labelStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              time9,
-                              style: labelStyle,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            //3
-            Container(
-              height: 80,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text('Content for Expansion Tile $index'),
             ),
           ],
+          trailing: IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              removeExpansionTile(
+                  index - 1); // Resta 1 para obtener el ¨ªndice correcto
+            },
+          ),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        //0
+        Container(
+          height: 50,
+          color: null,
+        ),
+        //
+        //
+        SizedBox(
+          height: 60,
+          width: MediaQuery.of(context).size.width,
+          //color: Colors.pink.shade100,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(88, 9, 88, 9),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.grey.shade300,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(2, 0, 3, 0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.26,
+                      child: ElevatedButton(
+                        onPressed: toggleButtonColor,
+                        style: ElevatedButton.styleFrom(
+                          elevation: isButtonActive ? 0 : 1,
+                          backgroundColor: isButtonActive
+                              ? Colors.grey.shade300
+                              : Colors.white,
+                          foregroundColor: Colors.black,
+                        ),
+                        child: const Text('Program'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(3, 0, 2, 0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.26,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          toggleButtonColor();
+                          createExpansionTile();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          elevation: isButtonActive ? 1 : 0,
+                          backgroundColor: isButtonActive
+                              ? Colors.white
+                              : Colors.grey.shade300,
+                          foregroundColor: Colors.black,
+                        ),
+                        child: const Text('History'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        //2
+        Expanded(
+  child: SingleChildScrollView(
+    child: Container(
+      child: Column(
+        children: List.generate(
+          expansionTiles.length,
+          (tileIndex) { // Cambia el nombre de la variable index aqu¨ª
+            if (tileIndex == 0) {
+              return Container(); // Mostrar un Container vac¨ªo en el ¨ªndice 0
+            } else {
+              return Column(
+                children: [
+                  expansionTiles[tileIndex],
+                 const Divider(), // Opcional: agregar una l¨ªnea divisoria
+                ],
+              );
+            }
+          },
         ),
       ),
+    ),
+  ),
+),
+        //3
+        Container(
+          height: 80,
+        ),
+      ],
     );
   }
 }
