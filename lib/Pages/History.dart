1b7a6eaeffb8191ab78b91519c2d6281c9770026
Frida Sofía Andrade Sierra <frisoandrade@gmail.com>
2023-08-15 13:56:39 -0600

    avance de cambio de pages

--- lib/Pages/History.dart
+++ lib/Pages/History.dart
@@ -25,6 +25,8 @@ class HistoryContent extends StatefulWidget {
 
 class _HistoryContentState extends State<HistoryContent> {
   bool isButtonActive = true;
+  bool isProgramActive = true; // Agregado
+  bool isHistoryActive = false; // Agregado
   List<ExpansionTile> expansionTiles = [];
 
   int index = 0;
@@ -45,22 +47,22 @@ class _HistoryContentState extends State<HistoryContent> {
   }
 
   void createExpansionTile() {
-    int index = expansionTiles.length + 1;
+    int newIndex =
+        expansionTiles.length + 1; // Usar una nueva variable de ¨ªndice
     setState(() {
       expansionTiles.add(
         ExpansionTile(
-          title: Text('Expansion Tile $index'),
+          title: Text('Expansion Tile $newIndex'),
           children: [
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Text('Content for Expansion Tile $index'),
+              child: Text('Content for Expansion Tile $newIndex'),
             ),
           ],
           trailing: IconButton(
             icon: Icon(Icons.delete),
             onPressed: () {
-              removeExpansionTile(
-                  index - 1); // Resta 1 para obtener el ¨ªndice correcto
+              removeExpansionTile(index);
             },
           ),
         ),
@@ -79,8 +81,15 @@ class _HistoryContentState extends State<HistoryContent> {
           height: 50,
           color: null,
         ),
-        //
-        //
+        //1
+        SizedBox(
+          child: ElevatedButton(
+              onPressed: () {
+                createExpansionTile();
+              },
+              child: Text('add')),
+        ),
+        //2
         SizedBox(
           height: 60,
           width: MediaQuery.of(context).size.width,
@@ -101,7 +110,13 @@ class _HistoryContentState extends State<HistoryContent> {
                     child: SizedBox(
                       width: MediaQuery.of(context).size.width * 0.26,
                       child: ElevatedButton(
-                        onPressed: toggleButtonColor,
+                        onPressed: () {
+                          toggleButtonColor();
+                          setState(() {
+                            isProgramActive = true;
+                            isHistoryActive = false;
+                          });
+                        },
                         style: ElevatedButton.styleFrom(
                           elevation: isButtonActive ? 0 : 1,
                           backgroundColor: isButtonActive
@@ -119,8 +134,11 @@ class _HistoryContentState extends State<HistoryContent> {
                       width: MediaQuery.of(context).size.width * 0.26,
                       child: ElevatedButton(
                         onPressed: () {
+                          setState(() {
+                            isProgramActive = false;
+                            isHistoryActive = true;
+                          });
                           toggleButtonColor();
-                          createExpansionTile();
                         },
                         style: ElevatedButton.styleFrom(
                           elevation: isButtonActive ? 1 : 0,
@@ -138,35 +156,62 @@ class _HistoryContentState extends State<HistoryContent> {
             ),
           ),
         ),
-        //2
-        Expanded(
-  child: SingleChildScrollView(
-    child: Container(
-      child: Column(
-        children: List.generate(
-          expansionTiles.length,
-          (tileIndex) { // Cambia el nombre de la variable index aqu¨ª
-            if (tileIndex == 0) {
-              return Container(); // Mostrar un Container vac¨ªo en el ¨ªndice 0
-            } else {
-              return Column(
-                children: [
-                  expansionTiles[tileIndex],
-                 const Divider(), // Opcional: agregar una l¨ªnea divisoria
-                ],
-              );
-            }
-          },
-        ),
-      ),
-    ),
-  ),
-),
         //3
+        Expanded(
+          child: SingleChildScrollView(
+            child: isProgramActive
+                ? ProgramContainer(expansionTiles: expansionTiles)
+                : HistoryContainer(), // Mostrar el contenido relevante
+          ),
+        ),
+        //4
         Container(
           height: 80,
         ),
       ],
+    );
+  }
+}
+
+class ProgramContainer extends StatelessWidget {
+  const ProgramContainer({
+    super.key,
+    required this.expansionTiles,
+  });
+
+  final List<ExpansionTile> expansionTiles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          if (expansionTiles.isNotEmpty)
+            ...expansionTiles.asMap().entries.map((entry) {
+              final index = entry.key;
+              final tile = entry.value;
+              return Column(
+                children: [
+                  tile,
+                  Divider(), // Opcional: agregar una l¨ªnea divisoria
+                ],
+              );
+            }),
+        ],
+      ),
+    );
+  }
+}
+
+class HistoryContainer extends StatelessWidget {
+  const HistoryContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      color: Colors.pink.shade200,
+      child: const Text('Datos'),
     );
   }
 }
