1b2133620ed0a9adefe0150bce35ea10a61ec48a
Frida Sofía Andrade Sierra <frisoandrade@gmail.com>
2023-08-21 12:37:57 -0600

    cambios en history page para agregar expansionTile

--- lib/Pages/History.dart
+++ lib/Pages/History.dart
@@ -29,28 +29,128 @@ class _HistoryContentState extends State<HistoryContent> {
   bool isHistoryActive = false; // Agregado
   List<ExpansionTile> expansionTiles = [];
 
-  int index = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        //0
+        Container(
+          height: 50,
+          color: null,
+        ),
+        //1
+        SizedBox(
+          height: 60,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(88, 9, 88, 9),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.grey.shade300,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(2, 0, 3, 0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.26,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            isProgramActive = true;
+                            isHistoryActive = false;
+                            isButtonActive = false;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          elevation: isButtonActive ? 0 : 1,
+                          backgroundColor: isButtonActive
+                              ? Colors.grey.shade300
+                              : Colors.white,
+                          foregroundColor: Colors.black,
+                        ),
+                        child: const Text('Program'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(3, 0, 2, 0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.26,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            isProgramActive = false;
+                            isHistoryActive = true;
+                            isButtonActive = true;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          elevation: isButtonActive ? 1 : 0,
+                          backgroundColor: isButtonActive
+                              ? Colors.white
+                              : Colors.grey.shade300,
+                          foregroundColor: Colors.black,
+                        ),
+                        child: const Text('History'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        //2
+        Expanded(
+          child: SingleChildScrollView(
+            child: isProgramActive
+                ? ProgramContainer(expansionTiles: expansionTiles)
+                : HistoryContainer(), // Mostrar el contenido relevante
+          ),
+        ),
+        //3
+        Container(
+          height: 80,
+        ),
+      ],
+    );
+  }
+}
+
+class ProgramContainer extends StatefulWidget {
+  const ProgramContainer({
+    super.key,
+    required this.expansionTiles,
+  });
+
+  final List<ExpansionTile> expansionTiles;
 
   @override
-  void toggleButtonColor() {
-    setState(() {
-      isButtonActive = !isButtonActive;
-    });
-  }
+  State<ProgramContainer> createState() => _ProgramContainerState();
+}
+
+class _ProgramContainerState extends State<ProgramContainer> {
+  int index = 0;
 
   void removeExpansionTile(int index) {
     setState(() {
-      if (index >= 0 && index < expansionTiles.length) {
-        expansionTiles.removeAt(index);
+      if (index >= 0 && index < widget.expansionTiles.length) {
+        widget.expansionTiles.removeAt(index);
       }
     });
   }
 
   void createExpansionTile() {
     int newIndex =
-        expansionTiles.length + 1; // Usar una nueva variable de ¨ªndice
+        widget.expansionTiles.length + 1; // Usar una nueva variable de ¨ªndice
     setState(() {
-      expansionTiles.add(
+      widget.expansionTiles.add(
         ExpansionTile(
           title: Padding(
             padding: const EdgeInsets.fromLTRB(15, 0, 20, 0),
@@ -83,124 +183,20 @@ class _HistoryContentState extends State<HistoryContent> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        //0
-        Container(
-          height: 50,
-          color: null,
-        ),
-        //1
-        /*SizedBox(
-          child: ElevatedButton(
-              onPressed: () {
-                createExpansionTile();
-              },
-              child: Text('add')),
-        ),*/
-        //2
-        SizedBox(
-          height: 60,
-          width: MediaQuery.of(context).size.width,
-          //color: Colors.pink.shade100,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(88, 9, 88, 9),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                color: Colors.grey.shade300,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(2, 0, 3, 0),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.26,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          toggleButtonColor();
-                          createExpansionTile();
-                          setState(() {
-                            isProgramActive = true;
-                            isHistoryActive = false;
-                          });
-                        },
-                        style: ElevatedButton.styleFrom(
-                          elevation: isButtonActive ? 0 : 1,
-                          backgroundColor: isButtonActive
-                              ? Colors.grey.shade300
-                              : Colors.white,
-                          foregroundColor: Colors.black,
-                        ),
-                        child: const Text('Program'),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(3, 0, 2, 0),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.26,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            isProgramActive = false;
-                            isHistoryActive = true;
-                          });
-                          toggleButtonColor();
-                        },
-                        style: ElevatedButton.styleFrom(
-                          elevation: isButtonActive ? 1 : 0,
-                          backgroundColor: isButtonActive
-                              ? Colors.white
-                              : Colors.grey.shade300,
-                          foregroundColor: Colors.black,
-                        ),
-                        child: const Text('History'),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        //3
-        Expanded(
-          child: SingleChildScrollView(
-            child: isProgramActive
-                ? ProgramContainer(expansionTiles: expansionTiles)
-                : HistoryContainer(), // Mostrar el contenido relevante
-          ),
-        ),
-        //4
-        Container(
-          height: 80,
-        ),
-      ],
-    );
-  }
-}
-
-class ProgramContainer extends StatelessWidget {
-  const ProgramContainer({
-    super.key,
-    required this.expansionTiles,
-  });
-
-  final List<ExpansionTile> expansionTiles;
-
-  @override
-  Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width * 0.9,
       child: Column(
         children: [
-          if (expansionTiles.isNotEmpty)
-            ...expansionTiles.asMap().entries.map((entry) {
+          //1
+          SizedBox(
+            child: ElevatedButton(
+                onPressed: () {
+                  createExpansionTile();
+                },
+                child: Text('add')),
+          ),
+          if (widget.expansionTiles.isNotEmpty)
+            ...widget.expansionTiles.asMap().entries.map((entry) {
               final index = entry.key;
               final tile = entry.value;
               return Column(
